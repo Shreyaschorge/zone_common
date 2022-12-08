e9a9ce765f83102ec817b6ac377b70e1a54df83a
Shreyaschorge <shreyaschorge81@gmail.com>
2022-12-08 11:41:22 +0530

    Add exceptions

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='zone-common',
+    version='0.0.1',
+    description='Common utility package for zone c2c',
+    author='Shreyas Chorge',
+    author_email='shreyaschorge81@gmail.com',
+    packages=["exceptions"],
+    package_dir={'': 'src'},
+    install_requires=['werkzeug',
+                      'flask-restful'
+                      ],
+)
---
+++ src/exceptions/unauthorized_exception.py
@@ -0,0 +1,12 @@
+from exceptions import CustomException
+
+
+class UnauthorizedException(CustomException):
+
+    status_code = 401
+
+    def __init__(self):
+        super().__init__("Unauthorized")
+
+    def serialize_errors(self):
+        return [{"message": "Not authorized to access the resourse"}]
