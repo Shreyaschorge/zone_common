028b02835be82c67dc2b72aaac217dfcf473bc4f
Shreyaschorge <shreyaschorge81@gmail.com>
2022-12-09 09:37:10 +0530

    clean build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zone-common',
-    version='0.0.6',
+    version='0.0.1',
     description='Common utility package for zone c2c',
     author='Shreyas Chorge',
     author_email='shreyaschorge81@gmail.com',
