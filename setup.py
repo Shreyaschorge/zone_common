422164b9953e8a337766d7d5f75a4b66055951a5
Shreyaschorge <shreyaschorge81@gmail.com>
2022-12-14 16:05:30 +0530

    Add base_listener

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='zone-common',
-    version='0.0.9',
+    version='0.0.10',
     description='Common utility package for zone c2c',
     author='Shreyas Chorge',
     author_email='shreyaschorge81@gmail.com',
     url='https://github.com/Shreyaschorge/zone_common',
-    packages=["zone_common/exceptions"],
+    packages=["zone_common/exceptions", "zone_common/events"],
     package_dir={'': 'src'},
     install_requires=['werkzeug',
-                      'flask-restful'
+                      'nats-py'
+                      'asyncio'
                       ],
 )
