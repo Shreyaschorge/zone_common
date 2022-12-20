4eb05274ee00d21cd04c37a4dd25cd98470ad7db
Shreyaschorge <shreyaschorge81@gmail.com>
2022-12-20 09:04:29 +0530

    Add Order Status

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zone-common',
-    version='0.0.15',
+    version='0.0.16',
     description='Common utility package for zone c2c',
     author='Shreyas Chorge',
     author_email='shreyaschorge81@gmail.com',
